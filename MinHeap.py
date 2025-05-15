5470682be4fdadf7a609e971db114e5cdf3c3507
ramchandra3101 <ramchandra3101@gmail.com>
2025-05-15 09:37:31 -0400

    developing Heap

--- MinHeap.py
+++ MinHeap.py
@@ -1,5 +1,48 @@
 class heapq:
-    def __init__(self, nums):
+    def leftNode(self, i):
+        return (2*i +1)
+    
+    def rightNode(self, i):
+        return (2*i + 2)
+
+    def parentNode(self, i):
+        return (i//2)-1
+    
+    def hasleft(self, i, arr):
+        return self.leftNode(i)<len(arr)
+    
+    def hasright(self, i, arr):
+        return self.rightNode(i)<len(arr)
+    
+    def hasParent(self, i):
+        return self.parentNode(i)>=0
+    
+    def heappop(self, arr):
+        return arr.pop(0)
+    
+    def swap(self, val1, val2):
+        temp = val1
+        val1 = val2
+        val2 = temp
+    
+    def heapify(self, arr):
+       
+
+
+
+
+        
+      
+
+
+        
+    
+
+    
+
+    
+
+
         
         
 
